972e4ddda8d26b1a43a8b4d2fca7f391bb4911e9
MJackson22-bit <jm5444859@gmail.com>
2022-05-22 12:06:12 -0600

    Add class CNRol in CapaNeogcio for get Rol

---
+++ CapaNegocio/CNRol.cs
@@ -0,0 +1,19 @@
+﻿using CapaDatos;
+using CapaEntidad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocio
+{
+    public class CNRol
+    {
+        private CNRol objectRol = new CNRol();
+        public List<Rol> listar()
+        {
+            return objectRol.listar();
+        }
+    }
+}
