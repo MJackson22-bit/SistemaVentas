44d2cdd335692f758e842c8af1fa90bca03a92fb
MJackson22-bit <jm5444859@gmail.com>
2022-05-22 14:57:15 -0600

    Rules of bussines

--- CapaNegocio/CNUsuario.cs
+++ CapaNegocio/CNUsuario.cs
@@ -17,14 +17,55 @@ namespace CapaNegocio
         }
         public int Registar(Usuario usuario, out string message)
         {
-            return objectUsuario.Registrar(usuario, out message);
+            message = string.Empty;
+            if(usuario.NombreCompleto == "")
+            {
+                message += "Es necesario el nombre de usuario\n";
+            }
+            if (usuario.Documento == "")
+            {
+                message += "Es necesario el documento de usuario\n";
+            }
+            if (usuario.Clave == "")
+            {
+                message += "Es necesario la clave del usuario\n";
+            }
+            if(message != string.Empty)
+            {
+                return 0;
+            }
+            else
+            {
+                return objectUsuario.Registrar(usuario, out message);
+            }
         }
         public bool Editar(Usuario usuario, out string message)
         {
-            return objectUsuario.Editar(usuario, out message);
+            message = string.Empty;
+            if (usuario.NombreCompleto == "")
+            {
+                message += "Es necesario el nombre de usuario\n";
+            }
+            if (usuario.Documento == "")
+            {
+                message += "Es necesario el documento de usuario\n";
+            }
+            if (usuario.Clave == "")
+            {
+                message += "Es necesario la clave del usuario\n";
+            }
+            if (message != string.Empty)
+            {
+                return false;
+            }
+            else
+            {
+                return objectUsuario.Editar(usuario, out message);
+            }
         }
         public bool Eliminar(Usuario usuario, out string message)
         {
+            message = string.Empty;
             return objectUsuario.Eliminar(usuario, out message);
         }
     }
