19d47e0b39d6bc63fc29beb4faf3c88d0b1e41c1
MJackson22-bit <jm5444859@gmail.com>
2022-05-23 20:16:32 -0600

    Cap of datascadd functionality add, edit, to list and delete

---
+++ CapaDatos/CDCategoria.cs
@@ -0,0 +1,131 @@
+﻿using CapaEntidad;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class CDCategoria
+    {
+        public List<Categoria> listar()
+        {
+            List<Categoria> lista = new List<Categoria>();
+            using (SqlConnection connection = new SqlConnection(Conexion.cadena))
+            {
+                try
+                {
+                    StringBuilder query = new StringBuilder();
+                    query.AppendLine("select IdCategoria, Descripcion, Estado from CATEGORIA");
+                    SqlCommand cmd = new SqlCommand(query.ToString(), connection);
+                    cmd.CommandType = CommandType.Text;
+                    connection.Open();
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            lista.Add(new Categoria()
+                            {
+                                IdCategoria = Convert.ToInt32(dr["IdCategoria"]),
+                                Descripcion = dr["Descripcion"].ToString(),
+                                Estado = Convert.ToBoolean(dr["Estado"])
+                                
+                            });
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    lista = null;
+                }
+            }
+            return lista;
+        }
+
+        public int Registrar(Categoria categoria, out string message)
+        {
+            int idiCategoriaGenerado = 0;
+            message = string.Empty;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(Conexion.cadena))
+                {
+                    SqlCommand cmd = new SqlCommand("SP_REGISTRARCATEGORIA", connection);
+                    cmd.Parameters.AddWithValue("Documento", categoria.Descripcion);
+                    cmd.Parameters.Add("Resultado", SqlDbType.Int).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+                    idiCategoriaGenerado = Convert.ToInt32(cmd.Parameters["IdCategoriaResultado"].Value);
+                    message = cmd.Parameters["Mensaje"].Value.ToString();
+                }
+            }
+            catch (Exception e)
+            {
+                idiCategoriaGenerado = 0;
+                message = e.Message;
+            }
+            return idiCategoriaGenerado;
+        }
+
+        public bool Editar(Categoria categoria, out string message)
+        {
+            bool respuesta = false;
+            message = string.Empty;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(Conexion.cadena))
+                {
+                    SqlCommand cmd = new SqlCommand("SP_EDITARCATEGORIA", connection);
+                    cmd.Parameters.AddWithValue("IdCategoria", categoria.IdCategoria);
+                    cmd.Parameters.AddWithValue("Descripcion", categoria.Descripcion);
+                    cmd.Parameters.Add("Resultado", SqlDbType.Int).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+                    respuesta = Convert.ToBoolean(cmd.Parameters["Respuesta"].Value);
+                    message = cmd.Parameters["Mensaje"].Value.ToString();
+                }
+            }
+            catch (Exception e)
+            {
+                respuesta = false;
+                message = e.Message;
+            }
+            return respuesta;
+        }
+
+
+        public bool Eliminar(Categoria categoria, out string message)
+        {
+            bool respuesta = false;
+            message = string.Empty;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(Conexion.cadena))
+                {
+                    SqlCommand cmd = new SqlCommand("SP_ELIMINARCATEGORIA", connection);
+                    cmd.Parameters.AddWithValue("IdCategoria", categoria.IdCategoria);
+                    cmd.Parameters.Add("Respuesta", SqlDbType.Int).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+                    respuesta = Convert.ToBoolean(cmd.Parameters["Respuesta"].Value);
+                    message = cmd.Parameters["Mensaje"].Value.ToString();
+                }
+            }
+            catch (Exception e)
+            {
+                respuesta = false;
+                message = e.Message;
+            }
+            return respuesta;
+        }
+    }
+}
