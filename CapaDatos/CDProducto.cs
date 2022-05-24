e05340d5ec462f3daaa0567fc88bedcfaa1e7c34
MJackson22-bit <jm5444859@gmail.com>
2022-05-23 23:11:24 -0600

    Add Functions for VRUD in CDProducto

---
+++ CapaDatos/CDProducto.cs
@@ -0,0 +1,149 @@
+﻿using CapaEntidad;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class CDProducto
+    {
+        public List<Producto> listar()
+        {
+            List<Producto> lista = new List<Producto>();
+            using (SqlConnection connection = new SqlConnection(Conexion.cadena))
+            {
+                try
+                {
+                    StringBuilder query = new StringBuilder();
+                    query.AppendLine("select IdProducto, Codigo, Nombre, Descripcion[DescripcionCategoria], IdCategoria, Stock, Precio_Compra, Precio_Venta from PRODUCTO p");
+                    query.AppendLine("INNER JOIN CATEGORIA c ON c.IdCategoria = p.IdCategoria");
+                    SqlCommand cmd = new SqlCommand(query.ToString(), connection);
+                    cmd.CommandType = CommandType.Text;
+                    connection.Open();
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            lista.Add(new Producto()
+                            {
+                                IdProducto = Convert.ToInt32(dr["IdProducto"]),
+                                Codigo = dr["Codigo"].ToString(),
+                                Nombre = dr["Nombre"].ToString(),
+                                Descripcion = dr["Descripcion"].ToString(),
+                                OCategoria = new Categoria()
+                                {
+                                    IdCategoria = Convert.ToInt32(dr["IdCategoria"]),
+                                    Descripcion = dr["DescripcionCategoria"].ToString()
+                                },
+                                Stock = Convert.ToInt32(dr["Stock"]),
+                                PrecioCompra = Convert.ToInt32(dr["PrecioCompra"].ToString()),
+                                PrecioVenta = Convert.ToInt32(dr["PrecioVenta"].ToString()),
+                                Estado = Convert.ToBoolean(dr["Estado"]),
+                            });
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    lista = null;
+                }
+            }
+            return lista;
+        }
+
+        public int Registrar(Producto producto, out string message)
+        {
+            int idiProductoGenerado = 0;
+            message = string.Empty;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(Conexion.cadena))
+                {
+                    SqlCommand cmd = new SqlCommand("SP_REGISTRARPRODUCTO", connection);
+                    cmd.Parameters.AddWithValue("Codigo", producto.Codigo);
+                    cmd.Parameters.AddWithValue("Nombre", producto.Nombre);
+                    cmd.Parameters.AddWithValue("Descripcion", producto.Descripcion);
+                    cmd.Parameters.AddWithValue("IdCategoria", producto.OCategoria.IdCategoria);
+                    cmd.Parameters.AddWithValue("Estado", producto.Estado);
+                    cmd.Parameters.Add("Resultado", SqlDbType.Int).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+                    idiProductoGenerado = Convert.ToInt32(cmd.Parameters["Resultado"].Value);
+                    message = cmd.Parameters["Mensaje"].Value.ToString();
+                }
+            }
+            catch (Exception e)
+            {
+                idiProductoGenerado = 0;
+                message = e.Message;
+            }
+            return idiProductoGenerado;
+        }
+
+        public bool Editar(Producto producto, out string message)
+        {
+            bool respuesta = false;
+            message = string.Empty;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(Conexion.cadena))
+                {
+                    SqlCommand cmd = new SqlCommand("SP_MODIFICARPRODUCTO", connection);
+                    cmd.Parameters.AddWithValue("IdProducto", producto.IdProducto);
+                    cmd.Parameters.AddWithValue("Codigo", producto.Codigo);
+                    cmd.Parameters.AddWithValue("Nombre", producto.Nombre);
+                    cmd.Parameters.AddWithValue("Descripcion", producto.Descripcion);
+                    cmd.Parameters.AddWithValue("IdCategoria", producto.OCategoria.IdCategoria);
+                    cmd.Parameters.AddWithValue("Estado", producto.Estado);
+                    cmd.Parameters.Add("Resultado", SqlDbType.Int).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+                    respuesta = Convert.ToBoolean(cmd.Parameters["Resultado"].Value);
+                    message = cmd.Parameters["Mensaje"].Value.ToString();
+                }
+            }
+            catch (Exception e)
+            {
+                respuesta = false;
+                message = e.Message;
+            }
+            return respuesta;
+        }
+
+
+        public bool Eliminar(Producto producto, out string message)
+        {
+            bool respuesta = false;
+            message = string.Empty;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(Conexion.cadena))
+                {
+                    SqlCommand cmd = new SqlCommand("SP_ELIMINARPRODUCTO", connection);
+                    cmd.Parameters.AddWithValue("IdProducto", producto.IdProducto);
+                    cmd.Parameters.Add("Respuesta", SqlDbType.Int).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+                    respuesta = Convert.ToBoolean(cmd.Parameters["Respuesta"].Value);
+                    message = cmd.Parameters["Mensaje"].Value.ToString();
+                }
+            }
+            catch (Exception e)
+            {
+                respuesta = false;
+                message = e.Message;
+            }
+            return respuesta;
+        }
+    }
+}
